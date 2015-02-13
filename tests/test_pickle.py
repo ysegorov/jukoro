cad01a71a9b329fcefe37522ca2ab325ac9d5ab0
Egorov Yuri <ysegorov@gmail.com>
2015-02-14 00:14:44 +0300

    pickle tests

--- tests/test_pickle.py
+++ tests/test_pickle.py
@@ -1,11 +1,24 @@
 # -*- coding: utf-8 -*-
 
+from unittest import TestCase
+
 from jukoro import pickle
+from jukoro.structures import ObjectDict
 
 
-def test():
-    d = {'a': 12, 'b': 13}
-    dumped = pickle.dumps(d)
-    restored = pickle.loads(dumped)
+class TestPickle(TestCase):
 
-    assert d == restored
+    def test_dict(self):
+        a = {'a': 12, 'b': 13}
+        b = pickle.dumps(a)
+        c = pickle.loads(b)
+
+        self.assertEqual(c, a)
+
+    def test_object_dict(self):
+        a = ObjectDict(a=12, b=24)
+        b = pickle.dumps(a)
+        c = pickle.loads(b)
+
+        self.assertIsInstance(c, ObjectDict)
+        self.assertEqual(c, a)
