23d6891f8edd2f1223c3b6ea46bc77bd1bf58914
Egorov Yuri <ysegorov@gmail.com>
2015-03-11 08:55:46 +0300

    psycopg2 registrations

--- jukoro/pg/__init__.py
+++ jukoro/pg/__init__.py
@@ -1,5 +1,13 @@
 # -*- coding: utf-8 -*-
 
+from __future__ import absolute_import
+
+import psycopg2
+import psycopg2.extensions
+import psycopg2.extras
+
+from jukoro import json
+
 from jukoro.pg.attrs import Attr, AttrDescr
 from jukoro.pg.db import PgDbPool, PgConnection, PgTransaction, PgResult
 from jukoro.pg.entity import BaseEntity, BaseUser
@@ -8,3 +16,17 @@ from jukoro.pg.exceptions import (
     PgConnectionClosedError)
 from jukoro.pg.introspect import inspect
 from jukoro.pg.utils import pg_uri_to_kwargs
+
+
+class JukoroPgJson(psycopg2.extras.Json):
+    def dumps(self, obj):
+        return json.dumps(obj)
+
+
+psycopg2.extensions.register_type(psycopg2.extensions.UNICODE)
+psycopg2.extensions.register_type(psycopg2.extensions.UNICODEARRAY)
+
+psycopg2.extensions.register_adapter(dict, JukoroPgJson)
+psycopg2.extras.register_default_json(globally=True, loads=json.loads)
+psycopg2.extras.register_default_jsonb(globally=True, loads=json.loads)
+psycopg2.extras.register_uuid()
