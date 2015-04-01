d6703512850be1fa05f228b9ef4531a27c0483c1
Egorov Yuri <ysegorov@gmail.com>
2015-04-01 10:17:34 +0300

    jukoro.arrow docstrings

--- jukoro/arrow/__init__.py
+++ jukoro/arrow/__init__.py
@@ -1,4 +1,24 @@
 # -*- coding: utf-8 -*-
+"""
+Module to work with datetimes using :class:`JuArrow <jukoro.arrow.JuArrow>`
+derived from :class:`Arrow <arrow.Arrow>`
+
+Usage examples::
+
+    >>> from jukoro import arrow
+    >>> arrow.utcnow()
+    <JuArrow [2015-04-01T07:12:50.969849+00:00]>
+    >>> arrow.utcnow().db_val()
+    '1427872406346229'
+    >>> arrow.utcnow().json_val()
+    '2015-04-01T07:13:31.532397+00:00'
+    >>> a = arrow.utcnow()
+    >>> b = a.db_val()
+    >>> a == arrow.from_db_val(b)
+    True
+
+
+"""
 
 from __future__ import absolute_import
 
--- jukoro/arrow/factory.py
+++ jukoro/arrow/factory.py
@@ -1,4 +1,8 @@
 # -*- coding: utf-8 -*-
+"""
+Provides factory to construct :class:`JuArrow <jukoro.arrow.JuArrow>` instances
+
+"""
 
 from __future__ import absolute_import, division
 
@@ -11,18 +15,39 @@ _factory = arrow.ArrowFactory(JuArrow)
 
 
 def get(*args, **kwargs):
+    """
+    Create :class:`JuArrow <jukoro.arrow.JuArrow>` instance using
+    factory's ``get`` method
+
+    """
     return _factory.get(*args, **kwargs)
 
+# convience name for json deserialization
 from_json_val = get
 
 
 def now(*args, **kwargs):
+    """
+    Create :class:`JuArrow <jukoro.arrow.JuArrow>` instance using
+    factory's ``now`` method
+
+    """
     return _factory.now(*args, **kwargs)
 
 
 def utcnow(*args, **kwargs):
+    """
+    Create :class:`JuArrow <jukoro.arrow.JuArrow>` instance using
+    factory's ``utcnow`` method
+
+    """
     return _factory.utcnow(*args, **kwargs)
 
 
 def from_db_val(val):
+    """
+    Create :class:`JuArrow <jukoro.arrow.JuArrow>` from integer stored in
+    PostgreSQL
+
+    """
     return get(int(val) / PRECISION)
