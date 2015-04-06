f4a922d0dfc7a82110f7fd487863708d48c57a55
Egorov Yuri <ysegorov@gmail.com>
2015-04-06 10:40:41 +0300

    jukoro.redis.db docstrings

--- jukoro/redis/db.py
+++ jukoro/redis/db.py
@@ -1,4 +1,8 @@
 # -*- coding: utf-8 -*-
+"""
+Abstraction to work with Redis connection
+
+"""
 
 from __future__ import absolute_import
 
@@ -18,6 +22,10 @@ return 0
 
 
 class Lua(object):
+    """
+    Lua script to be registered within Redis abstraction
+
+    """
 
     def __init__(self):
         self.strict_release = None
@@ -27,6 +35,14 @@ class Lua(object):
 
 
 class RedisDb(object):
+    """
+    ``StrictRedis`` proxy supporting namespaced ``Redis`` keys and custom
+    ``Lua`` scripts (not extendable yet)
+
+    :param uri:     connection uri
+    :param ns:      namespace for keys
+
+    """
 
     def __init__(self, uri, ns='app'):
         self._db = None
@@ -36,15 +52,31 @@ class RedisDb(object):
 
     @property
     def db(self):
+        """
+        Initializes redis connection if not initialized yet
+
+        :returns:   connection
+        :rtype:     instance of ``redis.StrictRedis``
+
+        """
         if self._db is None:
             self._db = redis.StrictRedis.from_url(self._uri)
             self._lua.register(self._db)
         return self._db
 
     def key(self, name):
+        """
+        Generates namespaced key
+
+        :rtype:     str
+
+        """
         return '{}:{}'.format(self._ns, name)
 
     def strict_release(self, keys, args):
+        """
+        Calls registered ``Lua`` script
+        """
         return self._lua.strict_release(keys=keys, args=args)
 
     def __getattr__(self, name):
